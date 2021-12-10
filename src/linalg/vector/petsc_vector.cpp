b59c9493b607c7e063fd4b8eba41115f82b21b84
Jacob Faibussowitsch <jacob.fai@gmail.com>
2021-12-10 16:58:30 -0500

    fix 3: a good day to fix

--- src/linalg/vector/petsc_vector.cpp
+++ src/linalg/vector/petsc_vector.cpp
@@ -8,12 +8,15 @@ sf_error_t petsc_vector::initialize_() noexcept
   if (vec_) return 0;
   SFCHECK(VecCreate(comm_,&vec_));
   SFCHECK(VecSetType(vec_,type_));
+  SFCHECK(VecSetSizes(vec_,heightl_,heightg_));
   return 0;
 }
 
 sf_error_t petsc_vector::assemble() noexcept
 {
   SFCHECK(initialize_());
+  SFCHECK(VecAssemblyBegin(vec_));
+  SFCHECK(VecAssemblyEnd(vec_));
   return 0;
 }
 
