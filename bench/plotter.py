5751b0447ffe070d2b27c50b7a7c2934e7103ef4
Jacob Faibussowitsch <jacob.fai@gmail.com>
2021-12-17 11:36:50 -0500

    plotter

--- bench/plotter.py
+++ bench/plotter.py
@@ -5,6 +5,25 @@
 """
 import collections
 import matplotlib.pyplot as plt
+import numpy as np
+
+def plot(x,data,sub,title,xlabel,ylabel):
+  for key,val in data.items():
+    if key == 'dense':
+      label = 'canonical 1-D column'
+    else:
+      label = 'fast 2-D upper triangular'
+    plt.loglog(x,val[sub],label=label,marker='x')
+  plt.ylabel(ylabel)
+  plt.xlabel(xlabel)
+  plt.title(title)
+  plt.grid()
+  plt.legend()
+  ax = plt.gca()
+  ax.yaxis.set_tick_params(which='major',size=8,width=2,direction='in',right='on')
+  ax.yaxis.set_tick_params(which='minor',size=4,width=1,direction='in',right='on')
+  plt.show()
+  return
 
 def main(infile):
   data = collections.defaultdict(list)
@@ -20,7 +39,18 @@ def main(infile):
       mat_size.append(int(entries[0]))
       num_procs.append(int(entries[1]))
       dim_r.append(int(entries[2]))
-  import ipdb; ipdb.set_trace()
+  mat_size  = np.array(mat_size)
+  num_procs = np.array(num_procs)
+  dim_r     = np.array(dim_r)
+  for k,v in data.items():
+    data[k] = {'time' : np.array(v)}
+    dk = data[k]
+    data[k]['speedup'] = dk['time'][0]/dk['time']
+    data[k]['efficiency'] = np.divide(dk['speedup'],num_procs)
+  suffix    = infile.stem.split('_')[1].title()
+  plot(num_procs,data,'speedup',f'Strong Scaling Speedup - {suffix}','ranks','speedup')
+  plot(num_procs,data,'efficiency',f'Strong Scaling Efficiency - {suffix}','ranks','efficiency')
+  plot(num_procs,data,'time',f'Execution Time - {suffix}','ranks','time [s]')
   return
 
 if __name__ == '__main__':
